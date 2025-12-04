6c43d8212ebf8b0c469ca30d33b10dedd427111a
HoffYoung <haofeng.yang17@gmail.com>
2025-12-04 23:30:02 +0100

    [Serving] adapt vllm configs for 1-node Qwen3-Next-80B-A3B-Instruct serving

--- scripts/datagen-noapi/measure_tp.sh
+++ scripts/datagen-noapi/measure_tp.sh
@@ -9,6 +9,6 @@ python async_requests.py --model=swiss-ai/Apertus-70B-Instruct-2509 --swissai_ap
 ## Olmo-3-7B-Instruct, local vLLM
 python async_requests.py --model=allenai/Olmo-3-7B-Instruct --swissai_api=none --base_url=http://172.28.36.112:8080/v1 --output_name=LongForm-Olmo-3-7B-Instruct --concurrency=128
 ## Qwen3-Next-80B-A3B-Instruct, local vLLM
-python async_requests.py --model=Qwen/Qwen3-Next-80B-A3B-Instruct --swissai_api=none --base_url=http://172.28.46.188:8080/v1 --output_name=LongForm-Qwen3-Next-80B-A3B-Instruct --concurrency=128
+python async_requests.py --model=Qwen/Qwen3-Next-80B-A3B-Instruct --swissai_api=none --base_url=http://172.28.42.88:8080/v1 --output_name=LongForm-Qwen3-Next-80B-A3B-Instruct --concurrency=128
 ## Olmo-3-32B-Think, local vLLM
 python async_requests.py --model=allenai/Olmo-3-32B-Think --swissai_api=none --base_url=http://172.28.46.124:8080/v1 --output_name=LongForm-Olmo-3-32B-Think --concurrency=128
